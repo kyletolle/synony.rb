941320a86a2b3e3852deb4e9be7d2d1bf80f49f8
Kyle Tolle <kyle@nullsix.com>
2017-09-12 17:59:39 -0700

    Add version number and command

--- lib/synony/cli.rb
+++ lib/synony/cli.rb
@@ -3,11 +3,13 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
+require 'synony/version'
+
 module Synony
   class CLI < Thor
     WORDS_TO_SKIP = ['a', 'the', 'of', 'an']
 
-    desc "replace", "Replace text in STDIN with a synonym, returning the resulting string on STDOUT"
+    desc 'replace', 'Replace text in STDIN with a synonym, returning the resulting string on STDOUT'
     def replace
       split_up_text = STDIN.read.split(/(\W)/)
 
@@ -27,6 +29,11 @@ module Synony
         end
 
       STDOUT.write(replaced_text.join(''))
+    end
+
+    desc 'version', 'Return the version of this library'
+    def version
+      puts Synony::VERSION
     end
   end
 end
---
+++ lib/synony/version.rb
@@ -0,0 +1,4 @@
+module Synony
+  VERSION = '0.0.1'
+end
+
